439bec7c3d208dd8ece73eacd32da20db78f13a1
yahalim <xalimonchuk@gmail.com>
2025-04-16 08:22:34 +0400

    test

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -4,6 +4,6 @@ from Mailing import Mailing
 from_address = Address("789456","Тольятти","Ленинский","21","90")
 to_address = Address("123456","Самара","Авроры","119","80")
 
-mailing = Mailing(to_address,from_address,555,'AAA123456')
+mailing = Mailing('AAA123456',to_address,from_address,555)
 
 print(mailing)
