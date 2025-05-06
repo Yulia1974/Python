a1016892976cd9411b1ab83ff0fb824edf0c78b6
yahalim <xalimonchuk@gmail.com>
2025-05-06 17:30:29 +0400

    test

--- 06_lesson/lesson06_task2.py
+++ 06_lesson/lesson06_task2.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 driver = webdriver.Chrome()
 
@@ -13,8 +14,11 @@ try:
 
     blue_button = driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary")
     blue_button.click()
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".btn.btn-primary"), "SkyPro")
 
-    button_text = WebDriverWait(driver, 20).until(
+    time.sleep(5)
+
+    button_text = WebDriverWait(driver, 60).until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, ".btn.btn-primary"))
     ).text
 
--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -2,14 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 driver = webdriver.Chrome()
 
 try:
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+    images = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.ID, "text"))
+    )
 
-    wait = WebDriverWait(driver, 10)
-    award_element = wait.until(EC.presence_of_element_located((By.ID, "award")))
+    time.sleep(20)
+
+    find= WebDriverWait(driver, 10)
+    award_element = find.until(EC.presence_of_element_located((By.ID, "award")))
     src = award_element.get_attribute("src")
     print(src)
 
